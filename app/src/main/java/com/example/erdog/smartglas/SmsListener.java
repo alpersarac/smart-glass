627e8ce5cd7f9374eef35ad27879d98bbe4390e2
Alper SARAÇ <50145163+alpersarac@users.noreply.github.com>
2020-03-10 18:30:30 +0300

    Create SmsListener.java

---
+++ app/src/main/java/com/example/erdog/smartglas/SmsListener.java
@@ -0,0 +1,52 @@
+package com.example.erdog.smartglas;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+
+public class SmsListener extends BroadcastReceiver {
+
+    private SharedPreferences preferences;
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+       CustomProcess cp=new CustomProcess();
+        // TODO Auto-generated method stub
+
+        if(intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")){
+            Bundle bundle = intent.getExtras();           //---get the SMS message passed in---
+            SmsMessage[] msgs = null;
+            String msg_from;
+            String msgBody=null;
+
+            Intent x = new Intent(context, CustomProcess.class);
+
+
+            if (bundle != null){
+                //---retrieve the SMS message received---
+                try{
+                    Object[] pdus = (Object[]) bundle.get("pdus");
+                    msgs = new SmsMessage[pdus.length];
+                    for(int i=0; i<msgs.length; i++){
+                        msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+                        msg_from = msgs[i].getOriginatingAddress();
+                        msgBody = msgs[i].getMessageBody();
+
+
+                    }
+
+                }catch(Exception e){
+//                            Log.d("Exception caught",e.getMessage());
+                }
+
+            }
+
+
+
+        }
+    }
+}
